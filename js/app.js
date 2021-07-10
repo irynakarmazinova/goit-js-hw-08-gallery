179841e89cc05406cf9cdd26340e4b23680587d4
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-07-10 20:51:20 +0300

    внесла изменения, не отображалось в гите на живой страничке

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-const galleryItems = [
+// const galleryItems = [
+export default [
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
@@ -275,4 +276,4 @@ const galleryItems = [
   },
 ];
 
-export default galleryItems;
+// export default galleryItems;
